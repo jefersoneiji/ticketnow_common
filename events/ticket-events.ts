69ec7bd95f671551d34f1855b69515fa433fb35e
Jeferson E <jeferson.eiji@icloud.com>
2025-02-09 20:08:22 -0300

    feat(common)!: added created and cancelled subjects

--- events/ticket-events.ts
+++ events/ticket-events.ts
@@ -14,3 +14,18 @@ export interface ITicketUpdated<T> {
     subject: Subjects.TicketUpdated,
     data: T & IEvent
 }
+
+export interface IOrderCreated<T> {
+    subject: Subjects.OrderCreated,
+    data: T & IEvent
+}
+
+export interface IOrderUpdated<T> {
+    subject: Subjects.OrderUpdated,
+    data: T & IEvent
+}
+
+export interface IOrderCancelled<T> {
+    subject: Subjects.OrderCancelled,
+    data: T & IEvent
+}
